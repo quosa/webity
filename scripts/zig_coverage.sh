04489ff61634dce429817b803843743596908151
Jussi Kuosa <jussiku@unity3d.com>
2025-09-01 00:50:30 +0300

    clean up zig coverage script

--- scripts/zig_coverage.sh
+++ scripts/zig_coverage.sh
@@ -1,14 +1,9 @@
 #! /bin/bash
 
 # SEE: https://ziggit.dev/t/using-kcov-with-zig-test/3421/5
-rm -rf coverage/* || mkdir coverage
-# zig test --test-no-exec -femit-bin=game_core_test src/core/game_core_test.zig
-# ./game_core_test
-# # kcov --clean --include-pattern='src/core/*.zig' coverage ./game_core_test
-# # kcov --clean --include-pattern="src/core/*.zig" coverage ./game_core_test
-# # kcov --clean --include-pattern="src/core/game_core.zig" coverage ./game_core_test
-# kcov --clean --include-pattern=src/core coverage ./game_core_test
-# open coverage/index.html
+rm -fr game_core_test*
+rm -fr game_engine_test*
+rm -rf coverage/*
 
 mkdir -p coverage/game_core_test
 mkdir -p coverage/game_engine_test
@@ -21,5 +16,5 @@ kcov --include-pattern=src/core/game_engine.zig coverage/game_engine_test ./game
 kcov --merge coverage coverage/game_core_test coverage/game_engine_test
 
 open coverage/index.html
-
-
+rm -fr game_core_test*
+rm -fr game_engine_test*
