84aced619ce15ba6f2f4270b45e6caee93f59ee3
Jussi Kuosa <jussiku@unity3d.com>
2025-08-31 22:45:12 +0300

    improved claude code launcher with mitm proxy and models

--- cc.sh
+++ cc.sh
@@ -33,7 +33,9 @@ fi
 # mitm
 # export http_proxy=http://127.0.0.1:8080
 # export https_proxy=http://127.0.0.1:8080
-
+export HTTP_PROXY="http://127.0.0.1:8080"
+export HTTPS_PROXY="http://127.0.0.1:8080"
+export NODE_EXTRA_CA_CERTS="/Users/jussiku/.mitmproxy/mitmproxy-ca-cert.pem"
 
 # Start Claude Code
 claude "$@"
